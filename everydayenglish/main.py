af2914ec854346dae73012ef924323d15748b051
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-01-09 10:20:19 -0800

    Delete unnecessary codes and organize

--- everydayenglish/main.py
+++ everydayenglish/main.py
@@ -1,12 +1,8 @@
 from word import Word
-from math import sqrt
 from random import randint
-from kivy.metrics import dp
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Rectangle, Point, GraphicException
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
 import csv
 
@@ -15,13 +11,13 @@ __version__ = '1.0'
 import kivy
 kivy.require('1.0.6')
 
+
 class EverydayEnglish(Widget):
 
     JAPANESE_FONT_NAME = 'ヒラギノ丸ゴ ProN W4.ttc'
     SWIPE_WIDTH = 50
 
     def prepare(self):
-
         self.orientation = 'vertical'
 
         with open('english.csv') as f:
@@ -29,57 +25,48 @@ class EverydayEnglish(Widget):
             self.word_matrix = [row for row in reader]
 
         self.title_label = Label(font_size=70,
-                            color=(1, 0, 0, 1),
-                            halign='left',
-                            valign='top',
-                            font_name=self.JAPANESE_FONT_NAME
-                            )
+                                 color=(1, 0, 0, 1),
+                                 halign='left',
+                                 valign='top',
+                                 font_name=self.JAPANESE_FONT_NAME
+                                 )
         self.add_widget(self.title_label)
 
         self.meaning_label = Label(font_size=50,
-                    color=(0, 1, 0, 1),
-                    halign='left',
-                    valign='top',
-                    font_name=self.JAPANESE_FONT_NAME
-                    )
+                                   color=(0, 1, 0, 1),
+                                   halign='left',
+                                   valign='top',
+                                   font_name=self.JAPANESE_FONT_NAME
+                                   )
         self.add_widget(self.meaning_label)
 
         self.sentence_label = Label(font_size=50,
-                    color=(1, 1, 1, 1),
-                    halign='left',
-                    valign='top',
-                    font_name=self.JAPANESE_FONT_NAME
-                    )
+                                    color=(1, 1, 1, 1),
+                                    halign='left',
+                                    valign='top',
+                                    font_name=self.JAPANESE_FONT_NAME
+                                    )
         self.add_widget(self.sentence_label)
 
         self.index = -1
         self.words = []
         self.display_next_word()
 
-    def normalize_pressure(self, pressure):
-        print(pressure)
-        # this might mean we are on a device whose pressure value is
-        # incorrectly reported by SDL2, like recent iOS devices.
-        if pressure == 0.0:
-            return 1
-        return dp(pressure * 10)
-
     def on_touch_down(self, touch):
         self.touch_down_x = touch.x
         self.touch_down_y = touch.y
 
-    def on_touch_move(self, touch):
-        pass
-
     def on_touch_up(self, touch):
-
-        if touch.x - self.touch_down_x > self.SWIPE_WIDTH: # swiped right
+        # swiped right
+        if touch.x - self.touch_down_x > self.SWIPE_WIDTH:
             self.display_previous_word()
-        elif self.touch_down_x - touch.x > self.SWIPE_WIDTH : # swiped left
+        # swiped left
+        elif self.touch_down_x - touch.x > self.SWIPE_WIDTH:
             self.display_next_word()
+        # just touched
         else:
             self.meaning_label.opacity = 100
-    
+
     def display_next_word(self):
         self.index += 1
 
@@ -88,11 +75,10 @@ class EverydayEnglish(Widget):
         elif self.index == len(self.words):
             word = self.pickup_word()
             self.words.append(word)
-            
+
         self.update_word_label(word)
 
     def display_previous_word(self):
-
         if self.index <= 0:
             pass
         else:
@@ -101,20 +87,21 @@ class EverydayEnglish(Widget):
             self.update_word_label(word)
 
     def update_word_label(self, word):
-        
+
         self.title_label.text_size = (self.width - 20, None)
         self.title_label.text = word.title
         self.title_label.texture_update()
         self.title_label.size = self.title_label.texture_size
         self.title_label.text_size = self.title_label.texture_size
         self.title_label.pos = (10, self.height - self.title_label.height)
-        
+
         self.meaning_label.text_size = (self.width - 20, None)
         self.meaning_label.text = word.meaning
         self.meaning_label.texture_update()
         self.meaning_label.size = self.meaning_label.texture_size
         self.meaning_label.text_size = self.meaning_label.texture_size
-        self.meaning_label.pos = (10, self.height - self.title_label.height - self.meaning_label.height)  
+        self.meaning_label.pos = (
+            10, self.height - self.title_label.height - self.meaning_label.height)
         self.meaning_label.opacity = 0
 
         self.sentence_label.text_size = (self.width - 20, None)
@@ -122,10 +109,10 @@ class EverydayEnglish(Widget):
         self.sentence_label.texture_update()
         self.sentence_label.size = self.sentence_label.texture_size
         self.sentence_label.text_size = self.sentence_label.texture_size
-        self.sentence_label.pos = (10, self.height - self.title_label.height - self.meaning_label.height - self.sentence_label.height)
+        self.sentence_label.pos = (10, self.height - self.title_label.height -
+                                   self.meaning_label.height - self.sentence_label.height)
 
     def pickup_word(self):
-
         while True:
             index = randint(3, len(self.word_matrix)-1)
             target_row = self.word_matrix[index]
@@ -144,11 +131,11 @@ class EverydayEnglish(Widget):
 
 
 class EverydayEnglishApp(App):
-    #title = 'EverydayEnglish'
-    #icon = 'icon.png'
+
+    # title = 'EverydayEnglish'
+    # icon = 'icon.png'
 
     def build(self):
-
         self.icon = 'icon.png'
         self.title = 'EverydayEnglish'
         self.main_frame = EverydayEnglish()
@@ -157,15 +144,10 @@ class EverydayEnglishApp(App):
 
     def on_start(self):
         super().on_start()
-        print("on_start")
         self.main_frame.prepare()
 
     def on_pause(self):
         return True
 
-
-
 if __name__ == '__main__':
     EverydayEnglishApp().run()
-
-
